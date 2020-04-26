4a937d86b65351d6ffed6bb2bfa878a23c1a1723
Leeny <leeny11@gmail.com>
2020-04-26 01:28:57 -0400

    newfile

---
+++ src/leeny.py
@@ -0,0 +1,10 @@
+print "Leeny"
+
+#i = 0
+#buffer = ""
+#while i < 100:
+#	print('Leeny: ' +repr(i))
+#	print "Leeny:\t\t{}".format(i)
+#	buffer += "Leeny:\t\t{}\n".format(i)
+#	i += 1
+#print(buffer)
